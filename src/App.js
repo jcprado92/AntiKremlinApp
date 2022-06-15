f834e1ad951e7075977f02ff542ef35de9848ab3
jede <jedebrahma@pursuit.org>
2022-06-14 22:02:06 -0400

    added Router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 import Navbar from './components/common/Navbar';
 import Footer from './components/common/Footer';
 import Home from './components/common/Home';
-import About from './components/common/About';
+// import About from './components/common/About';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <h1>This is our TeleApp</h1>
+      <Navbar />
+      <Routes>
+      <Home />
+      </Routes>
+      <Footer />
     </div>
+    </Router>
   );
 }
 
