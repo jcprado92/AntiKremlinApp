067501189e9de7bdcbe85760b366a2ba474f88b3
jede <jedebrahma@pursuit.org>
2022-06-15 21:36:28 -0400

    navbar links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './App.css';
 import Navbar from './components/common/Navbar';
-import Footer from './components/common/Footer';
-import Home from './components/common/Home';
-// import About from './components/common/About';
+// import Footer from './components/common/Footer';
+// import Home from './components/common/Home';
+import About from './components/common/About';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 
@@ -12,12 +12,12 @@ function App() {
   return (
     <Router>
     <div className="App">
-      <h1>This is our TeleApp</h1>
+      {/* <h1>This is our TeleApp</h1> */}
       <Navbar />
-      <Routes>
+      {/* <Routes>
       <Home />
       </Routes>
-      <Footer />
+      <Footer /> */}
     </div>
     </Router>
   );
--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,6 +1,9 @@
 const key = process.env.REACT_APP_API_KEY;
 
-fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${key}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    
+const getData = () => {
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${key}&q=kidcudi`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
+
+getData();
