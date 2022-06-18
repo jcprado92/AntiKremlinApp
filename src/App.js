0678aeabe0e393efd0633ed1fb2e25a6efd80663
John Prado <jcprado20@icloud.com>
2022-06-18 18:08:34 -0400

    adds videos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Navbar from "./components/common/Navbar";
 // import Footer from './components/common/Footer';
 import Home from "./components/common/Home";
+import VideosIndex from "./components/VideosIndex";
+
 
 // import About from './components/common/About';
 
@@ -12,32 +14,28 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const [search, setSearch] = useState('')
+  const [maxResult, setMaxResult] = useState(5)
 
   useEffect(() => {
    
     fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}&q=kidcudi`
+      `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}&q=${search}&maxResults=${maxResult}`
     )
-      .then((response) => {
-        const videoResponse = response.json();
-        console.log(videoResponse, response);
-     
-        return videoResponse;
-      })
-      .then((data) => {
-      
-        return setVideos(data)});
-  }, []);
+      .then((response) => response.json())
+      .then((data) => setVideos(data.items));
+  }, [search, maxResult]);
+
 
 
   return (
     <Router>
       <div className="App">
 
-        <Navbar />
+        <Navbar setSearch={setSearch} setMaxResult={setMaxResult}/>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/videos" element={<VideosIndex />} />
+          <Route path="/videos" element={<VideosIndex videos={videos}/>} />
         </ Routes>
         {/* <Footer /> */}
 
